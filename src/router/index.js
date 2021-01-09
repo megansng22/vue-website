4ce086e8a237feae6e6e1ff5ba858de8ffb1c0ff
megansng22 <72670496+megansng22@users.noreply.github.com>
2021-01-09 10:25:21 -0600

    Update index.js
    template

--- src/router/index.js
+++ src/router/index.js
@@ -12,23 +12,23 @@ const routes = [
   // },
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/home',
-    name: 'Home',
+    path: '/',
+    name: 'home',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "home" */ '../views/home.vue')
+    component: Home
   },
   {
     path: '/contactme',
-    name: 'ContactMe',
+    name: 'contactme',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -36,7 +36,7 @@ const routes = [
   },
   {
     path: '/introblurb',
-    name: 'IntroBlurb',
+    name: 'introblurb',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -44,7 +44,7 @@ const routes = [
   },
   {
     path: '/workexamples',
-    name: 'WorkExamples',
+    name: 'workexamples',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -53,7 +53,9 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
